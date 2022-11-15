65193a1614cbbf49fcdc0c844ed1ae3f3529681f
Ahmad Jamal <ahmadjamalcs@gmail.com>
2022-11-15 21:03:14 +0530

    delete function added

--- app.js
+++ app.js
@@ -60,8 +60,20 @@ app.put('/todos/:id/markAsCompleted', async (request, response)=>{
   }
 });
 
-app.delete('/todos/:id', (request, response)=>{
-  console.log('Delete a todo by id', request.params.id);
+// app.delete('/todos/:id', (request, response)=>{
+//   console.log('Delete a todo by id', request.params.id);
+// });
+
+app.delete('/todos/:id', async function(request, response) {
+  console.log('We have to delete a Todo with ID: ', request.params.id);
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our database to delete a Todo by ID.
+  // eslint-disable-next-line max-len
+  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
+  // response.send(true)
+  const deleteFlag = await Todo.destroy({where: {id: request.params.id}});
+  response.send(deleteFlag ? true : false);
 });
 
 module.exports = app;
